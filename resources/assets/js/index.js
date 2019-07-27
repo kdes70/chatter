2f8a3b0a78d9c92e2446a4056ead9740c2c6e807
kdes70 <kdes70@mail.ru>
2019-07-27 22:23:54 +0700

    index search

---
+++ resources/assets/js/index.js
@@ -0,0 +1,4 @@
+import Vue from "vue";
+
+Vue.component('conversations', require('./components/chatter/Conversations'));
+Vue.component('chat-messages', require('./components/chatter/ChatMessages'));
